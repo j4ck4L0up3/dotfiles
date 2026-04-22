9a51ea137cb2758e8c53784069769bf8c59765d7
Jack Gaskins <jackgaskins.tech@gmail.com>
2026-04-22 11:26:19 -0400

    setup universal clipboard for nvim

--- nvim/.config/nvim/lua/j4ck4L0up3/core/options.lua
+++ nvim/.config/nvim/lua/j4ck4L0up3/core/options.lua
@@ -34,6 +34,17 @@ opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line, or
 
 -- clipboard
 opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+vim.g.clipboard = {
+	name = "OSC 52",
+	copy = {
+		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+	},
+	paste = {
+		["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+		["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+	},
+}
 
 -- split windows
 opt.splitright = true -- split vertical window to the right

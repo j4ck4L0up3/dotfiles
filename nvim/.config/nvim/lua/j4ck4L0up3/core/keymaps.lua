85d487dddfe53a90aaab2278a1cbb20350d2883f
Jack Gaskins <jackgaskins.tech@gmail.com>
2026-08-29 18:57:09 -0400

    fix terminal close keymap in nvim

--- nvim/.config/nvim/lua/j4ck4L0up3/core/keymaps.lua
+++ nvim/.config/nvim/lua/j4ck4L0up3/core/keymaps.lua
@@ -24,4 +24,4 @@ keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
-keymap.set("t", "Escape", "[[<C-><C-n>]]", { desc = "Close terminal" }) -- close terminal
+keymap.set("t", "Escape", "<C-\\><C-n>", { desc = "Close terminal" }) -- close terminal

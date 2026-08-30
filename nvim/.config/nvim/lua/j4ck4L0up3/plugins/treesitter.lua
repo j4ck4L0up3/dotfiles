b0ff8c519b219b44a3fc80c8150a74f0a976fd79
Jack Gaskins <jackgaskins.tech@gmail.com>
2026-08-30 14:27:08 -0400

    fix treesitter bugs

--- nvim/.config/nvim/lua/j4ck4L0up3/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/j4ck4L0up3/plugins/treesitter.lua
@@ -1,66 +1,50 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	event = { "BufReadPre", "BufNewFile" },
+	branch = "main",
 	build = ":TSUpdate",
 	dependencies = {
 		"windwp/nvim-ts-autotag",
 	},
 	config = function()
-		local treesitter = require("nvim-treesitter.configs")
+		local parsers = {
+			"json",
+			"javascript",
+			"go",
+			"typescript",
+			"tsx",
+			"yaml",
+			"html",
+			"css",
+			"markdown",
+			"markdown_inline",
+			"sql",
+			"regex",
+			"bash",
+			"python",
+			"lua",
+			"vim",
+			"dockerfile",
+			"gitignore",
+			"query",
+			"cpp",
+			"c",
+			"vimdoc",
+			"templ",
+		}
 
-		-- configure treesitter
-		treesitter.setup({
-			auto_install = true,
+		require("nvim-treesitter").install(parsers)
 
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-
-			-- enable indentation
-			indent = { enable = true },
-
-			-- enable autotagging (w/ nvim-ts-autotag plugin)
-			autotag = {
-				enable = true,
-			},
-
-			-- ensure these language parsers are installed
-			ensure_installed = {
-				"json",
-				"javascript",
-				"go",
-				"typescript",
-				"tsx",
-				"yaml",
-				"html",
-				"css",
-				"markdown",
-				"sql",
-				"regex",
-				"bash",
-				"python",
-				"lua",
-				"vim",
-				"dockerfile",
-				"gitignore",
-				"query",
-				"cpp",
-				"c",
-				"vimdoc",
-				"templ",
-			},
-
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = "<C-space>",
-					node_decremental = "<C-b>",
-				},
-			},
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = parsers,
+			callback = function(args)
+				vim.treesitter.start(args.buf)
+				vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+				vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
 		})
+
+		require("nvim-ts-autotag").setup()
 
 		vim.filetype.add({
 			extension = {
